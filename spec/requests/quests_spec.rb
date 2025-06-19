aea5ec8969047063d2d6a4a42496d41ef14c0321
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-19 10:38:45 +0700

    test: add POST /quests for test create new quest

--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Quests", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/quests/create"
-      expect(response).to have_http_status(:success)
+  let(:valid_attributes) { { quest: { title: "New Quest" } } }
+  let(:invalid_attributes) { { quest: { title: nil } } }
+  describe "POST /quests" do\
+    it "creates a new quest with valid attributes" do
+      expect {
+        post quests_path, params: valid_attributes
+      }.to change(Quest, :count).by(1)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:notice]).to eq("Quest was successfully created.")
+    end
+
+    it "does not create a new quest with invalid attributes" do
+      expect {
+        post quests_path, params: invalid_attributes
+      }.not_to change(Quest, :count)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to eq("Failed to create quest.")
     end
   end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/quests/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/quests/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
