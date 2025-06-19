b757888781bd48bdd29d136d36ae9106eab6b11b
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-19 11:00:00 +0700

    test: quests controller when create a quest

---
+++ spec/requests/quests_controller_spec.rb
@@ -0,0 +1,22 @@
+describe QuestsController, type: :controller do
+  describe "POST #create" do
+    let(:valid_attributes) { { quest: { title: "New Quest" } } }
+    let(:invalid_attributes) { { quest: { title: nil } } }
+
+    it "creates a new quest with valid attributes" do
+      expect {
+        post :create, params: valid_attributes
+      }.to change(Quest, :count).by(1)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:notice]).to eq("Quest was successfully created.")
+    end
+
+    it "does not create a new quest with invalid attributes" do
+      expect {
+        post :create, params: invalid_attributes
+      }.not_to change(Quest, :count)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to eq("Failed to create quest.")
+    end
+  end
+end
