b331e228ffdfb6bab1d129f4b992bbeffff52f6d
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-19 10:20:12 +0700

    test: pages => when request to root path

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /index" do
-    it "returns http success" do
+    it "returns http redirect" do
       get "/pages"
       expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe "GET /" do
+    it "returns http success" do
+      get "/"
+      expect(response).to have_http_status(:success)
     end
   end
 
