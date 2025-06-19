6b779122d6402a93e9d3ded9c45f2cc5da2339c0
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-19 10:39:16 +0700

    add: logic for create quest on Quest controller

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,5 +1,11 @@
 class QuestsController < ApplicationController
   def create
+    quest = Quest.new(quest_params)
+    if quest.save
+      redirect_to root_path, notice: "Quest was successfully created."
+    else
+      redirect_to root_path, alert: "Failed to create quest."
+    end
   end
 
   def update
@@ -7,4 +13,9 @@ class QuestsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def quest_params
+      params.require(:quest).permit(:title)
+    end
 end
