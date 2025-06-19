c0dd003d42bbb5ca5df2e36941b18d8ae7985944
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-19 10:29:48 +0700

    refactor: as name on /pages in routes config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "pages#index"
 
   resources :quests, only: [ :create, :update, :destroy ]
-  get "/pages", to: redirect("/"), as: :quests
+  get "/pages", to: redirect("/"), as: :index
   get "/brag", to: "pages#brag", as: :brag
   get "up" => "rails/health#show", as: :rails_health_check
 end
